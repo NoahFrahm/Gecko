a9845a2b534d3ebb50d09b2a58ec92fed99783fd
NoahFrahm <nofrahm@live.unc.edu>
2022-05-16 16:13:58 -0700

    added xcode proj
    created base xcode project

---
+++ lizard/lizard/lizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  lizardApp.swift
+//  lizard
+//
+//  Created by Noah Frahm on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct lizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
